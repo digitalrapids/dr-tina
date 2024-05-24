3169b8f6335ddc29236e81aeba8aede47b46f552
jraedisch <jasper@raedisch.net>
2024-05-24 15:59:31 +0200

    publish docs just to check

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,4 +11,9 @@ export default function (eleventyConfig) {
   eleventyConfig.addJavaScriptFunction("drExists", (thing) => !!thing);
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("assets");
+  return {
+    dir: {
+      output: 'docs'
+    }
+  }
 }
